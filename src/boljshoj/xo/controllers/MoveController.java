4763a4a5806208d19c8e6761327bb34af9df3a91
boljshoj@gmail.com <123qwepioneeraZd>
2017-09-27 12:26:54 +0300

    MoveController - created

---
+++ src/boljshoj/xo/controllers/MoveController.java
@@ -0,0 +1,22 @@
+package boljshoj.xo.controllers;
+
+import boljshoj.xo.model.Field;
+import boljshoj.xo.model.Figure;
+import boljshoj.xo.model.exceptions.AlreadyOccupiedException;
+import boljshoj.xo.model.exceptions.InvalidPointException;
+
+import java.awt.*;
+
+public class MoveController {
+
+    public void applyFigure(final Field field,
+                            final Point point,
+                            final Figure figure) throws InvalidPointException,
+                                                        AlreadyOccupiedException{
+        if (field.getFigure(point) != null) {
+            throw new AlreadyOccupiedException();
+        }
+        field.setFigure(point, figure);
+    }
+
+}
